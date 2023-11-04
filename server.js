5bb31321c7f5a1025903d3f853b943a3223eb160
hafizhuzaifaahmed <huzaifaahmed90000@gmail.com>
2023-11-04 12:31:23 +0500

    Node.js and MongoDB connection setup

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import Colors from 'colors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
 
 //configure env
 dotenv.config();
 
+//database config
+connectDB();
+
 //rest object
 const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
 
 //rest api
 app.get('/',(req,res)=>{
