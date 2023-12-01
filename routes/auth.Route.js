4084f74a46d1919629e4d435fd31d7dcdd1dd91d
hafizhuzaifaahmed <huzaifaahmed90000@gmail.com>
2023-12-01 20:55:28 +0500

    login API

--- routes/auth.Route.js
+++ routes/auth.Route.js
@@ -1,10 +1,13 @@
  import  Express  from "express";
- import {registerController} from '../controllers/authController.js';
+ import {registerController, loginController} from '../controllers/authController.js';
  //router object
  const router = Express.Router()
 
  //routing
  //REGISTER || METHOD POST
- router.post('/register', registerController)
+ router.post('/register', registerController);
+
+ //LOGIN || POST
+ router.post('/login', loginController)
 
  export default router
